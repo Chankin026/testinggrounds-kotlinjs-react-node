dcfe69b077cc527ba2c6a3e825ba33a1d53d7534
Jens Brimfors <jens.brimfors@nordnet.se>
2019-01-09 17:32:07 +0100

    Moved files to /backend

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,3 +1,69 @@
+import com.moowork.gradle.node.npm.NpmTask
+import com.moowork.gradle.node.task.NodeTask
+import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
+
 buildscript {
-    println("I'm backend")
+    dependencies {
+        classpath("com.moowork.gradle:gradle-node-plugin:1.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.10")
+    }
+    println("Hello from backend")
+}
+
+plugins {
+    id("kotlin2js") version "1.3.10"
+    id("com.moowork.node") version "1.2.0"
+}
+
+dependencies {
+    implementation(kotlin("stdlib-js"))
+    testImplementation(kotlin("test-js"))
+}
+
+repositories {
+    jcenter()
+}
+
+tasks {
+    node {
+        download = true
+    }
+    compileKotlin2Js {
+        kotlinOptions {
+            moduleKind = "commonjs"
+            destinationDir = file("$buildDir/js")
+            //outputFile = "$buildDir/js/output.js"
+            noStdlib = false
+        }
+    }
+    compileTestKotlin2Js {
+        kotlinOptions {
+            moduleKind = "commonjs"
+            outputFile = "$buildDir/jstest.js"
+            noStdlib = false
+        }
+    }
+    val populateNodeModules by registering(Copy::class) {
+        group = "build"
+        description = "Assemble the web application"
+        dependsOn("compileKotlin2Js")
+        from("$buildDir/js")
+
+        configurations.testCompile.get().forEach {
+            from(zipTree(it.absolutePath).filter { f -> f.name.endsWith(".js") })
+        }
+
+        into("$buildDir/node_modules")
+    }
+    val runJasmine by registering(NodeTask::class) {
+        dependsOn(compileTestKotlin2Js, populateNodeModules, npmInstall)
+        setScript(file("node_modules/jasmine/bin/jasmine.js"))
+        setArgs(listOf("$buildDir/jstest"))
+    }
+    test {
+        dependsOn(runJasmine)
+    }
+    create("run", NpmTask::class) {
+        setArgs(listOf("start"))
+    }
 }
